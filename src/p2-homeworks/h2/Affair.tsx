c6b9c0b786e2b599663d53c87127965087eb2b03
Kostya Lapatin <klrotex11@gmail.com>
2022-06-09 23:53:14 +0300

    Second (Priority)

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
+import {AffairType} from "./HW2";
+import classes from "./Affairs.module.css";
 
 type AffairPropsType = {
     // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+    affair: AffairType // need to fix any
+    deleteAffairCallback: (_id: number) => void// need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+    const deleteCallback = (id: number) => {
+        {props.deleteAffairCallback(id)}
+    }// need to fix
 
     return (
         <div>
-            // show some text
+            <div>
+                <span className={classes.someClass}>{props.affair.name}</span>
+                <span>{props.affair.priority}</span>
+                <button onClick={()=>deleteCallback(props.affair._id)}>X</button>
+            </div>
 
-            <button onClick={deleteCallback}>X</button>
+
         </div>
     )
 }
